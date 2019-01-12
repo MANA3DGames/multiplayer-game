1b5e7da0e006a19dd486c4216037d8ea62b5c6fb
DESKTOP-NCLNINC\Mahmoud <mahmoud.abuobaid@outlook.com>
2019-01-12 13:52:18 +0200

    Player Simple Setup
    - Player Label
    - Change player color (Only local)

--- MultiplayerGame/Assets/_Scripts/PlayerSetup.cs
+++ MultiplayerGame/Assets/_Scripts/PlayerSetup.cs
@@ -1,16 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
 public class PlayerSetup : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Color m_playerColor;
+	public string m_basename = "PLAYER";
+	public int m_playerNum = 1;
+	public Text m_playerNameText;
+
+
+	void Start()
+	{
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public override void OnStartClient ()
+	{
+		base.OnStartClient ();
+		if (m_playerNameText != null)
+		{
+			m_playerNameText.enabled = false;
+		}
 	}
+		
+
+
+	public override void OnStartLocalPlayer()
+	{
+		base.OnStartLocalPlayer();
+
+		MeshRenderer[] meshes = GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer r in meshes)
+		{
+			r.material.color = m_playerColor;
+		}
+
+		if (m_playerNameText != null)
+		{
+			m_playerNameText.enabled = true;
+			m_playerNameText.text = m_basename + m_playerNum.ToString();
+
+		}
+
+
+	}
+
 }
