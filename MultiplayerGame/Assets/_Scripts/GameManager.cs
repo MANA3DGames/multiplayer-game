c14e8f054e36f95598ca97fcb84d1fdaaea02074
DESKTOP-NCLNINC\Mahmoud <mahmoud.abuobaid@outlook.com>
2019-01-17 08:54:37 +0200

    Added Main Game Loop

--- MultiplayerGame/Assets/_Scripts/GameManager.cs
+++ MultiplayerGame/Assets/_Scripts/GameManager.cs
@@ -6,6 +6,14 @@ using System.Collections;
 
 public class GameManager : NetworkBehaviour 
 {
+	public Text m_messageText;
+
+	int m_minPlayers = 2;
+	int m_maxPlayers = 4;
+
+	[SyncVar]
+	public int m_playerCount = 0;
+
 	static GameManager instance;
 
 	public static GameManager Instance
@@ -36,5 +44,67 @@ public class GameManager : NetworkBehaviour
 			Destroy(gameObject);
 		}
 	}
+
+	void Start()
+	{
+		StartCoroutine("GameLoopRoutine");
+	}
+
+	IEnumerator GameLoopRoutine()
+	{
+		yield return StartCoroutine("EnterLobby");
+		yield return StartCoroutine("PlayGame");
+		yield return StartCoroutine("EndGame");
+	}
+
+	IEnumerator EnterLobby()
+	{
+		if (m_messageText !=null)
+		{
+			m_messageText.gameObject.SetActive(true);
+			m_messageText.text = "waiting for players";
+		}
+
+		while (m_playerCount < m_minPlayers)
+		{
+			DisablePlayers();
+			yield return null;
+		}
+	}
+
+	IEnumerator PlayGame()
+	{
+		if (m_messageText !=null)
+		{
+			m_messageText.gameObject.SetActive(false);
+		}
+
+		yield return null;
+	}
+
+	IEnumerator EndGame()
+	{
+		yield return null;
+	}
+
+	void SetPlayerState(bool state)
+	{
+		PlayerController[] allPlayers = GameObject.FindObjectsOfType<PlayerController>();
+		foreach (PlayerController p in allPlayers)
+		{
+			p.enabled = state;
+		}
+	}
+
+	void EnablePlayers()
+	{
+		SetPlayerState(true);
+	}
+
+	void DisablePlayers()
+	{
+		SetPlayerState(false);
+	}
+
 
 }
--- MultiplayerGame/Assets/_Scripts/PlayerController.cs
+++ MultiplayerGame/Assets/_Scripts/PlayerController.cs
@@ -119,6 +119,8 @@ public class PlayerController : NetworkBehaviour {
 
 	}
 
+
+
 }
 
 
